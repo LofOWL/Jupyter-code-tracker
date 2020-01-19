f7160fe8a07650a100bbf2dc3d6344c3093b94ed
DongJun Jin <jeffersonjjin@gmail.com>
2020-01-19 17:59:56 -0500

    refrac main.py and create refresh function

--- main.py
+++ main.py
@@ -1,42 +1,74 @@
 from tkinter import *
+from tkinter import ttk
 from map import Map
 
 import pandas as pd
 
 from items import vs
 
+class Tool:
 
-def data():
-	pa = pd.read_csv("/Users/apple/Desktop/10118245/result1.csv")
-	#pa = pa[pa["status"] == "success"]
-	#all_list = list(pa["id"])
+	def __init__(self):
+		self.root = Tk()
+		self.screen_height = None
 
-	mp = Map("8e6d37c84f1e373c27dbd1db38d77005d5ab27d4#1")
-	return mp
+		self._init_root()
+		self.shaListBox = None
+		self.data = None
+		self.currentId = None
+		self._init_box()
+		self._init_button()
+		self.canvas = None
+		self._init_vs()
+
+	def _init_root(self):
+		self.screen_height = self.root.winfo_screenheight()
+		self.root.geometry("300x"+str(self.screen_height))
+
+	def _init_box(self):
+		def alist():
+			pa = pd.read_csv("/Users/apple/Desktop/10118245/result1.csv")
+			pa = pa[pa["status"] == "success"]
+			all_list = list(pa["id"])
+			return all_list
+		self.data = alist()
+		self.currentId = self.data[0]
+
+		self.shaList = ttk.Combobox(self.root, values=self.data, width=100)
+		self.shaList.current(1)
+		self.shaList.pack()
+
+	def _init_button(self):
+		def reload():
+			self.currentId = self.data[self.shaList.current()]
+			mp = Map(self.currentId)
+			self.canvas.refresh(mp)
+		change = Button(self.root, text="change", command=reload)
+		change.pack()
+
+
+	def _init_vs(self):
+
+		mp = Map(self.currentId)
+		self.canvas = vs(self.root,mp)
+
+		frame=Frame(self.root,width=200,height=self.screen_height)
+		frame.pack(expand=True, fill=BOTH) #.grid(row=0,column=0)
+
+		vbar=Scrollbar(frame,orient=VERTICAL)
+		vbar.pack(side=RIGHT,fill=Y)
+		vbar.config(command=self.canvas.yview)
+
+		self.canvas.configure(scrollregion = self.canvas.bbox("all"))
+		self.canvas.config(yscrollcommand=vbar.set)
+		self.canvas.pack(side=LEFT,expand=True,fill=BOTH)
 
 
 
-root = Tk()
+	def run(self):
+		self.root.mainloop()
 
 
-mp = data()
-
-canvas = vs(root,mp)
-
-screen_height = root.winfo_screenheight()
-
-frame=Frame(root,width=200,height=screen_height)
-frame.pack(expand=True, fill=BOTH) #.grid(row=0,column=0)
-
-vbar=Scrollbar(frame,orient=VERTICAL)
-vbar.pack(side=RIGHT,fill=Y)
-vbar.config(command=canvas.yview)
-
-canvas.configure(scrollregion = canvas.bbox("all"))
-canvas.config(yscrollcommand=vbar.set)
-canvas.pack(side=LEFT,expand=True,fill=BOTH)
-
-root.geometry("300x"+str(screen_height))
-
-
-root.mainloop()
+if __name__ == "__main__":
+	tool = Tool()
+	tool.run()
