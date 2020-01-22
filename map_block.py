445a3701f7dc584dd81c769dbb97354041abed04
DongJun Jin <jeffersonjjin@gmail.com>
2020-01-22 11:35:06 -0500

    add type_merge()

--- map_block.py
+++ map_block.py
@@ -1,5 +1,7 @@
 from map import Map
 from collections import Counter
+import pandas as pd
+import os
 
 
 class MapBlock:
@@ -12,6 +14,8 @@ class MapBlock:
 		self.parent_block = dict(Counter([ i.parent.block for i in self.data if i.parent.exist]))
 
 		self.child_block_first_line = [ i for i in self.data if i.child.exist and i.child.line == 1]
+
+		self.parent_block_first_line = [ i for i in self.data if i.parent.exist and i.parent.line == 1]
 
 	def type_total(self):
 		
@@ -40,8 +44,20 @@ class MapBlock:
 		for i in self.child_block_first_line:
 			c_block = i.child.block
 			bp = blockmap.mapChildParent(c_block)
-			if len(bp) != 1:
+			if len(bp) > 1:
 				split_block.append([c_block,bp])
+
+		return split_block
+
+	def type_merge(self):
+		blockmap = Block(self.data)
+		
+		split_block = []
+		for i in self.parent_block_first_line:
+			p_block = i.parent.block
+			bp = blockmap.mapParentChild(p_block)
+			if len(bp) > 1:
+				split_block.append([bp,p_block])
 
 		return split_block
 
@@ -52,12 +68,32 @@ class Block:
 		self.data = data
 
 	def mapChildParent(self,block):
-		filter_list = set([ i.parent.block for i in self.data if i.child.block == block])
+		filter_list = set([ i.parent.block for i in self.data if i.child.block == block and i.parent.block != -1])
 		return filter_list
 
+	def mapParentChild(self,block):
+		filter_list = set([ i.child.block for i in self.data if i.parent.block == block and i.child.block != -1])
+		return filter_list
+
+def data():
+	cwd = os.getcwd()
+	pa = pd.read_csv(cwd+"/10118245/result1.csv")
+	pa = pa[pa["status"]=="success"]
+	all_list = list(pa["id"])
+	return all_list
+
+def checkall():
+	for i in data():
+		mp = Map(i)
+		a = MapBlock(mp)
+		if len(a.type_merge()) >= 1:
+			print(i)
+
+
 if __name__ == "__main__":
-	mp = Map("4696d4bbcb4aff65c43cbed94ad060518dedc346#0")
+	mp = Map("7e104b2e83fc2386d98a4501ef6c6e3f9fb88a7b#0")
 	a = MapBlock(mp)
 	total = a.type_total()
 	print(total)
 	print(a.type_split())
+	print(a.type_merge())
